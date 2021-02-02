87a0643ed40717f37cd2303ea3de8da74d6c59ea
gusta <gusta@LAPTOP-LJC53O8C>
2021-02-02 16:16:59 -0600

    1.1

---
+++ src/mx/edu/cesba/model/Employee.java
@@ -0,0 +1,21 @@
+/*
+Nombre: Gustavo Navarrete Cruz
+Matricula: 2022431
+ */
+package mx.edu.cesba.model;
+
+public class Employee {
+
+    public int id;
+    public String firstname;
+    public String lastName;
+    public int age;
+    public int phoneNumber;
+    public double salary;
+    public String hiredate;
+
+    public void getDetails() {
+
+    }
+
+}
